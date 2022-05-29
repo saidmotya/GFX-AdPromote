f440db47807d601acf4569ba6414f44a343f781b
SAID MOTYA <zonek.app@hotmail.com>
2022-05-29 13:40:14 +0100

    Update GFX.java
    Add links json from github !

--- app/src/main/java/com/gfx/adPromote/ads/GFX.java
+++ app/src/main/java/com/gfx/adPromote/ads/GFX.java
@@ -9,10 +9,10 @@ public class GFX {
     public static String AppPromo = "https://raw.githubusercontent.com/saidmotya/GFX-AdPromote/master/ScreenShot/MyPromote.json";
 
     //Loading file with Youtube promote Only !
-    public static String YoutubePromo = "";
+    public static String YoutubePromo = "https://raw.githubusercontent.com/saidmotya/GFX-AdPromote/master/ScreenShot/youtubePromote.json";
 
     //Loading file with App promote and Youtube promote !
-    public static String AppWithYoutubePromo = "";
+    public static String AppWithYoutubePromo = "https://raw.githubusercontent.com/saidmotya/GFX-AdPromote/master/ScreenShot/PromoteAppWithYoutube.json";
 
     public static void setLog(String log){
         Log.d("gfx",log);
